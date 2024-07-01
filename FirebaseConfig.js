1d9c377932a929ca68b08fbab7079d96b7e46997
Lester <lesterondoy456@gmail.com>
2024-07-01 09:50:40 +0800

    initial commit

---
+++ FirebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCnWNq2NHZnkCKnY68XO485JjlTSkJp9e0",
+    authDomain: "website-1420c.firebaseapp.com",
+    databaseURL: "https://website-1420c-default-rtdb.firebaseio.com",
+    projectId: "website-1420c",
+    storageBucket: "website-1420c.appspot.com",
+    messagingSenderId: "340964025178",
+    appId: "1:340964025178:web:e97058d3c4df8ef11cc963",
+    measurementId: "G-506MW6GY94"
+};
